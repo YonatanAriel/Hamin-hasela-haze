0a4b341dd7397e95a282e300c862e5a068137475
Yonatan Ariel <yonatanriel@gmail.com>
2024-11-26 18:28:25 +0200

    adding english text to home page

--- src/data/languages/English.js
+++ src/data/languages/English.js
@@ -7,25 +7,29 @@ const English = {
   },
   home: {
     sections: {
-      title: "Everything you need to know about us",
+      title: "Everything You Need to Know About Us",
       section1: {
-        h1: "Economist Mody's Arrival",
-        p: `The genuine concern this time: After the Pitz Report, the treasury is preparing for the arrival of Economist Mody on the backdrop of the explanation that the relatively optimistic Pitz credit rating report was the result of an assessment that the legislation will not really be implemented. Senior officials in the treasury argue that Mody's next report, to be published in October, "could bring a tough decision to the economy." Mody's economists, who already lowered Israel's rating forecast last April, will be coming to Israel soon.`,
+        h1: "Comfort and Relaxation",
+        p: `Our living room is the perfect place to unwind. A cozy sofa, luxurious rug, and smart TV with a variety of content create a relaxing atmosphere.  
+  You'll also find a family game corner, movies for all ages, and creative kits that help bring family members closer together.`,
       },
       section2: {
-        h1: "Economist Mody's Arrival",
-        p: `The genuine concern this time: After the Pitz Report, the treasury is preparing for the arrival of Economist Mody on the backdrop of the explanation that the relatively optimistic Pitz credit rating report was the result of an assessment that the legislation will not really be implemented. Senior officials in the treasury argue that Mody's next report, to be published in October, "could bring a tough decision to the economy." Mody's economists, who already lowered Israel's rating forecast last April, will be coming to Israel soon.`,
+        h1: "A Warm Welcome",
+        p: `The entrance to the cabin greets you with a warm, inviting atmosphere. A colorful flower path and a cozy seating area set the tone for a relaxing stay.  
+  Your perfect drink is waiting for you at the coffee corner, featuring an espresso machine and a variety of capsules to choose from.`,
       },
       section3: {
-        h1: "Economist Mody's Arrival",
-        p: `The genuine concern this time: After the Pitz Report, the treasury is preparing for the arrival of Economist Mody on the backdrop of the explanation that the relatively optimistic Pitz credit rating report was the result of an assessment that the legislation will not really be implemented. Senior officials in the treasury argue that Mody's next report, to be published in October, "could bring a tough decision to the economy." Mody's economists, who already lowered Israel's rating forecast last April, will be coming to Israel soon.`,
+        h1: "Nature and Comfort",
+        p: `Our cabin blends comfort with natural beauty. Artificial flowers add a touch of color and tranquility to the design. The fully equipped kitchen makes meal prep easy and enjoyable, while the functional furniture complements the natural aesthetic. Every detail is carefully chosen to ensure a peaceful stay.`,
       },
       section4: {
-        h1: "Economist Mody's Arrival",
-        p: `The genuine concern this time: After the Pitz Report, the treasury is preparing for the arrival of Economist Mody on the backdrop of the explanation that the relatively optimistic Pitz credit rating report was the result of an assessment that the legislation will not really be implemented. Senior officials in the treasury argue that Mody's next report, to be published in October, "could bring a tough decision to the economy." Mody's economists, who already lowered Israel's rating forecast last April, will be coming to Israel soon.`,
+        h1: "Quiet and Peace",
+        p: `Our bedroom is designed to offer complete rest and relaxation. With a spacious double bed and quality linens, you’ll enjoy a perfect night’s sleep.  
+               The simple yet elegant design, paired with the intimate space, creates a calming atmosphere that helps you unwind after a busy day. It's the perfect place to relax and enjoy private moments in complete peace.`,
       },
     },
   },
+
   inTheArea: {
     sections: {
       title: "Discover what the area has to offer",
